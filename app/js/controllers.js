0fc1bbc190ac73b64adc64ccaea0c9295e9e1b73
Patrick Shouse <shouse.patrick@gmail.com>
2013-09-03 20:25:02 -0400

    updates

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,10 +2,22 @@
 
 /* Controllers */
 
+function MyCtrl1($scope, $http) {
+      $http.get('http://localhost:3001/members').success(function(data) {
+    $scope.members = data;
+  });
+ 
+  $scope.orderProp = 'name';
+}
+
+function MyCtrl2() {};
+
+/* remove template's controller defs
 angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
+  controller('MyCtrl1', [function($scope, $http) {
 
   }])
   .controller('MyCtrl2', [function() {
 
   }]);
+  */
